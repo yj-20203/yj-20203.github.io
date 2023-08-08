02719d6b0a7e202296a8b7f58a34bcbf3ce2d1e5
yj <teoyingjie@outlook.com>
2023-08-08 17:18:00 +0800

    pic

---
+++ WebDevA2/script.js
@@ -0,0 +1,48 @@
+//target all elements to save to constants
+const page1btn=document.querySelector("#page1btn");
+const page2btn=document.querySelector("#page2btn");
+const page3btn=document.querySelector("#page3btn");
+var allpages=document.querySelectorAll(".page");
+//select all subtopic pages
+
+console.log(allpages);
+hideall();
+
+function hideall(){ //function to hide all pages
+    for(let onepage of allpages){ //go through all subtopic pages
+        onepage.style.display="none"; //hide it
+    }
+}
+
+function show(pgno){ //function to show selected page no
+    hideall();
+    //select the page based on the parameter passed in
+    let onepage=document.querySelector("#page"+pgno);
+    //show the page
+    onepage.style.display="block";
+
+}
+
+/*Listen for clicks on the buttons, assign anonymous
+eventhandler functions to call show function*/
+page1btn.addEventListener("click", function () { 
+    show(1); 
+});
+page2btn.addEventListener("click", function () { 
+    show(2); 
+});
+page3btn.addEventListener("click", function () {
+    show(3); 
+});
+
+/*for hamMenu */
+const hamBtn=document.querySelector("#hamIcon");
+hamBtn.addEventListener("click",toggleMenus);
+const menuItemsList=document.querySelector("nav ul");
+
+function toggleMenus(){ /*open and close menu*/
+    menuItemsList.classList.toggle("menuHide");
+    // if(menuItemsList.style.display=="block")
+    //     menuItemsList.style.display="none";
+    // else menuItemsList.style.display="block";
+}//can optimize using toggle class with css transitions
